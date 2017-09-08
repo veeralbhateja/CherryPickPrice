44e65807c058b66a8109fb0f2bb20b7c617f7f32
Veeral Bahteja <veeral.bhateja@gmail.com>
2017-09-08 15:43:49 +0530

    initial

---
+++ CheryPickPrices/CheryPickPrices.h
@@ -0,0 +1,19 @@
+//
+//  CheryPickPrices.h
+//  CheryPickPrices
+//
+//  Created by Veeral Bhateja on 08/09/17.
+//  Copyright © 2017 Veeral Bhateja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CheryPickPrices.
+FOUNDATION_EXPORT double CheryPickPricesVersionNumber;
+
+//! Project version string for CheryPickPrices.
+FOUNDATION_EXPORT const unsigned char CheryPickPricesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheryPickPrices/PublicHeader.h>
+
+
